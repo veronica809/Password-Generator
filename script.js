b9e1f536c8fb3c3779db5cde0d126cd9c2a82927
Veronica Garcia <veronicagarcia@veronicas-mbp.attlocal.net>
2022-09-29 21:47:45 -0400

    added password length and password criteria

--- script.js
+++ script.js
@@ -5,7 +5,49 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
 var userInput = window.prompt("What is the password length?")
 
+//parseInt turns userInput into a number
+var passwordLength = parseInt(userInput)
+
+if(isNaN(passwordLength)){
+  window.alert("Not a number!")
+  return //Takes user back to initial code
 }
+
+//set condition for password length between 8-128 characters
+if (passwordLength < 8 || passwordLength > 128){
+  window.alert("Password must be between 8 and 128 characters")
+  return
+  }
+
+  //select password criteria
+var userWantLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+var userWantUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+var userWantNumbers = window.confirm("Would you like to include numbers in your password?")
+var userWantCharacter = window.confirm("Would you like to include special characters in your password?")
+
+//character lists
+var lowerCase = ["a","b","c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n" ,"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = []
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+//turn lowercase letters to uppercase
+for (var i = 0; i < lowerCase.length; i++) {
+  upperCase[i] = lowerCase[i].toUpperCase()
+}
+
+}
+
+
+//select password criteria
+var userWantLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+var userWantUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+var userWantNumbers = window.confirm("Would you like to include numbers in your password?")
+var userWantCharacter = window.confirm("Would you like to include special characters in your password?")
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
